7aed35a9953f56e8044f21b7d6033ba6441a593b
Kimdir Cha <80375500+kimdircha@users.noreply.github.com>
2021-08-30 23:33:20 +0500

    O'zgaruvchi nomlari tuzatildi

--- Son-topish-o_yini/son_topish.py
+++ Son-topish-o_yini/son_topish.py
@@ -3,54 +3,54 @@
     17.03.2021"""
 
 import random as rdm
-print(f"\nAssalomu aleykum. Keling, \"Son topish\" o'yinini o'ynaymiz.")
+print("\nAssalomu aleykum. Keling, \"Son topish\" o'yinini o'ynaymiz.")
 while True:
-    b=int(input("\nMen 0 va 10 oralig'idan bir son o'yladim. Uni topishga urinib ko'ring:\n>>> "))
-    n=0
-    a=rdm.randint(0,10)
+    playersGuess=int(input("\nMen 0 va 100 oralig'idan bir son o'yladim. Uni topishga urinib ko'ring:\n>>> "))
+    playersAttemps=0
+    computersChoise=rdm.randint(0,100)
     while True:
-        n+=1
-        if a==b:
-            print(f"To'g'ri, men o'ylagan son {a} edi.")
+        playersAttemps+=1
+        if computersChoise==playersGuess:
+            print(f"To'g'ri, men o'ylagan son {computersChoise} edi.")
             break
-        elif a<b:
-            b=int(input(f"O'ylagan sonim {b}dan kichik.\n>>> "))
+        elif computersChoise<playersGuess:
+            playersGuess=int(input(f"O'ylagan sonim {playersGuess}dan kichik.\n>>> "))
         else:
-            b=int(input(f"O'ylagan sonim {b}dan katta.\n>>> "))
-    input(f"Qoyil {n} ta urinishda topdingiz. "
+            playersGuess=int(input(f"O'ylagan sonim {playersGuess}dan katta.\n>>> "))
+    input(f"Qoyil {playersAttemps} ta urinishda topdingiz. "
                f"Endi siz son o'ylang va 'Enter'ni bosing. Men uni topishga urinib ko'raman.")
-    m=0
-    p=0
-    q=10
-    y=rdm.randint(p,q)
+    computersAttemps=0
+    lowerLimit=0
+    upperLimit=100
+    computersGuess=rdm.randint(lowerLimit,upperLimit)
     while True:
-        m+=1
-        tek=input(f"Siz o'ylagan son {y}mi?\n"
-        	          f"\nAgar siz o'ylagan son {y}dan katta bo'lsa + deb, kichik bo'lsa - deb, to'g'ri topgan bo'lsam ha deb yozing >>> ")
-        if tek=='+':
-            p=y+1
-            if p>q:
+        computersAttemps+=1
+        isEqual=input(f"Siz o'ylagan son {computersGuess}mi?\n"
+        	          f"\nAgar siz o'ylagan son {computersGuess}dan katta bo'lsa + deb, kichik bo'lsa - deb, to'g'ri topgan bo'lsam ha deb yozing >>> ")
+        if isEqual=='+':
+            lowerLimit=computersGuess+1
+            if lowerLimit>upperLimit:
                 print("\nYolg'on gapirdingiz! O'yin qoidasi buzildi.")
-                m=0
+                computersAttemps=0
                 break
-            y=rdm.randint(p,q)
-        elif tek=='-':
-            q=y-1
-            if p>q:
+            computersGuess=rdm.randint(lowerLimit,upperLimit)
+        elif isEqual=='-':
+            upperLimit=computersGuess-1
+            if lowerLimit>upperLimit:
                 print("\nYolg'on gapirdingiz! O'yin qoidasi buzildi.")
-                m=0
+                computersAttemps=0
                 break
-            y=rdm.randint(p,q)
+            computersGuess=rdm.randint(lowerLimit,upperLimit)
         else:
-            print(f"Men {m} ta urinishda topdim")
+            print(f"Men {computersAttemps} ta urinishda topdim")
             break
-    if n>m:
+    if playersAttemps>computersAttemps:
         print("Men g'olibman!")
-    elif n<m:
+    elif playersAttemps<computersAttemps:
         print("Siz g'olibsiz!")
     else:
         print("Durrang!")
-    oo=input("Yana o'ynaysizmi? (ha/yo'q)\n>>> ")
-    if oo!="ha":
+    playAgain=input("Yana o'ynaysizmi? (ha/yo'q)\n>>> ")
+    if playAgain!="ha":
         break
 print("O'yin tugadi!")
