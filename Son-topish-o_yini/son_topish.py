483941d5589e0df7d8d3b676a23400666bbb5d6d
Kimdir Cha <80375500+kimdircha@users.noreply.github.com>
2021-08-31 10:13:48 +0500

    Update son_topish.py

--- Son-topish-o_yini/son_topish.py
+++ Son-topish-o_yini/son_topish.py
@@ -8,29 +8,62 @@ import random as rdm
 
 # >\"Son topish\" o'yinini o'ynaymiz.
 # >\nMen 0 va 100 oralig'idan bir son o'yladim. Uni topishga urinib ko'ring:
+# ? config.FINDNUMBER[message.from_user.id]['playersAttemps'] = 0
+# ? config.FINDNUMBER[message.from_user.id]['computersChoise'] = rdm.randint(0,100)
+# ? computersAttemps=0
+# ? lowerLimit=0
+# ? upperLimit=100
+# ? computersGuess=rdm.randint(lowerLimit,upperLimit)
 
 # < number
 
-# ? try b = int msg.text
+# ? try config.FINDNUMBER[message.from_user.id]['playersGuess'] = int msg.text
 # ? except > qoida buzildi, yutdim / -> Games.game
-# ? else config.FINDNUMBER[message.from_user.id]['n'] = 0
+# ? else 
+# ? config.FINDNUMBER[message.from_user.id]['playersAttemps'] += 1
+# ? if computersChoise==playersGuess:
+#     > f"To'g'ri, men o'ylagan son {computersChoise} edi."
+#     ? -> Games.findPlayerssNumber
+#     > f"Qoyil {playersAttemps} ta urinishda topdingiz. "
+#                f"Endi siz son o'ylang va 'Enter'ni bosing. Men uni topishga urinib ko'raman."
+# ? elif computersChoise<playersGuess:
+#     > f"O'ylagan sonim {playersGuess}dan kichik.\n>>> "
+# ? else:
+#     f"O'ylagan sonim {playersGuess}dan katta.\n>>> "
 
+# < boshlash from buttton /state = Games.findPlayerssNumber
 
+# ? computersAttemps+=1
+# > f"Siz o'ylagan son {computersGuess}mi?\n"
+#         	          f"\nAgar siz o'ylagan son {computersGuess}dan katta bo'lsa + deb, kichik bo'lsa - deb, to'g'ri topgan bo'lsam ha deb yozing >>> "
+#     with inline buttons
 
+# if call.data=='+':
+#     lowerLimit=computersGuess+1
+#     if lowerLimit>upperLimit:
+#         > "\nYolg'on gapirdingiz! O'yin qoidasi buzildi."
+#         computersAttemps=0
+#         -> Games.game
+#     computersGuess=rdm.randint(lowerLimit,upperLimit)
+# elif call.data=='-':
+#     upperLimit=computersGuess-1
+#     if lowerLimit>upperLimit:
+#         > "\nYolg'on gapirdingiz! O'yin qoidasi buzildi."
+#         computersAttemps=0
+#         -> Games.game
+#     computersGuess=rdm.randint(lowerLimit,upperLimit)
+# else:
+#     > f"Men {computersAttemps} ta urinishda topdim"
+#     -> Games.game
 
+# if playersAttemps>computersAttemps:
+#     > "Men g'olibman!"
+# elif playersAttemps<computersAttemps:
+#     > "Siz g'olibsiz!"
+# else:
+#     > "Durrang!"
 
-
-
-
-
-
-
-
-
-
-
-
-
+# > "O'yin tugadi!"
 
 print("\nAssalomu aleykum. Keling, \"Son topish\" o'yinini o'ynaymiz.")
 while True:
